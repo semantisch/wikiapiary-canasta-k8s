dd6ecbfccefdc5ac17b4f91617dbc987420f959e
Daniel Dobriy <semantisch@users.noreply.github.com>
2026-05-02 19:05:00 +0000

    Fix Foreground top bar dropdown menus

--- legacy/foreground/assets/scripts/foreground.js
+++ legacy/foreground/assets/scripts/foreground.js
@@ -71,6 +71,89 @@ jQuery(document).ready(function() {
     });
   }
 
+  function bindTopBarDropdowns() {
+    var selector = '#top-bar-left > li.has-dropdown > a[href="#"]';
+    var $buttons = jQuery(selector);
+
+    function closeItem($item) {
+      var $button = $item.children('a').first();
+      var $menu = $item.children('.dropdown').first();
+
+      $item.removeClass('hover');
+      $button.removeClass('open').attr('aria-expanded', 'false');
+      $menu.removeClass('open').attr('aria-hidden', 'true').css({
+        display: 'none',
+        width: '',
+        height: '',
+        clip: '',
+        overflow: '',
+        left: '',
+        top: ''
+      });
+    }
+
+    function openItem($item) {
+      var $button = $item.children('a').first();
+      var $menu = $item.children('.dropdown').first();
+
+      $item.addClass('hover');
+      $button.addClass('open').attr('aria-expanded', 'true');
+      $menu.addClass('open').attr('aria-hidden', 'false').css({
+        display: 'block'
+      });
+    }
+
+    function closeAll(exceptItem) {
+      $buttons.each(function () {
+        var $item = jQuery(this).closest('li.has-dropdown');
+        if (!exceptItem || !$item.is(exceptItem)) {
+          closeItem($item);
+        }
+      });
+    }
+
+    $buttons.each(function (index) {
+      var $button = jQuery(this);
+      var $item = $button.closest('li.has-dropdown');
+      var $menu = $item.children('.dropdown').first();
+      var menuId;
+
+      if (!$menu.length) {
+        return;
+      }
+
+      menuId = $menu.attr('id') || ($item.attr('id') ? $item.attr('id') + '-menu' : 'foreground-topbar-menu-' + index);
+      $menu.attr({
+        id: menuId,
+        'aria-hidden': 'true'
+      });
+      $button.attr({
+        'aria-controls': menuId,
+        'aria-expanded': 'false'
+      });
+
+      closeItem($item);
+
+      $button.off('click.foregroundTopbar').on('click.foregroundTopbar', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        if ($item.hasClass('hover')) {
+          closeItem($item);
+        } else {
+          closeAll($item);
+          openItem($item);
+        }
+      });
+    });
+
+    jQuery(document).off('click.foregroundTopbar').on('click.foregroundTopbar', function (e) {
+      if (!jQuery(e.target).closest('#top-bar-left > li.has-dropdown').length) {
+        closeAll();
+      }
+    });
+  }
+
   // Log errors
   jQuery(document).foundation(function (response) {
     if (window.console) console.log(response.errors);
@@ -81,6 +164,7 @@ jQuery(document).ready(function() {
   bindExplicitDropdown('#actions-button', '#actions', 'foregroundActions');
   bindExplicitDropdown('#toolbox-button', '#toolbox-dropdown', 'foregroundToolbox');
   bindExplicitDropdown('#personal-tools-button', '#personal-tools-menu', 'foregroundPersonalTools');
+  bindTopBarDropdowns();
   
   // The Echo extension puts an item in personal tools that Foreground really should have in the top menu
   // to make this easier, we move it here and loaded earlier to speed up transform
